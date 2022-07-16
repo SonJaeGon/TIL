59c48cde97ed01a0c49361bcc44787386ba9d1f3
SonJaeGon <wprhs0650@gmail.com>
2022-07-16 17:02:40 +0900

    New file Created: Vimguide.py
    This file is guid to Vim

---
+++ python/vimguide.py
@@ -0,0 +1,14 @@
+print("Hello world")
+
+# print("hello world")
+# press "i", you can insert anything
+# press "esc", normal mode
+# press "shift + Y", corsor line copy
+# press "p", paste
+# press "dd", deleted
+# press "u", undo
+
+# normal mod-
+# :q = quit
+# :wq = write and quit
+# :q! = not write and quit
